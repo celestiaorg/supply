d17de645c14fd201e4c5c45b595394f0dad95d2a
Rootul Patel <rootulp@gmail.com>
2023-10-29 11:56:49 +0100

    feat: get supply by date

--- main.go
+++ main.go
@@ -9,20 +9,40 @@ import (
 )
 
 type response struct {
-	Available   int64 `json:"available"`
+	// Available is the total supply of utia available at the given time.
+	Available int64 `json:"available"`
+	// Circulating is the circulating supply of utia available at the given time.
 	Circulating int64 `json:"circulating"`
 }
 
-// getSupply responds with the list of all albums as JSON.
+// getSupply responds with the current supply of utia.
 func getSupply(c *gin.Context) {
+	t := time.Now()
 	c.IndentedJSON(http.StatusOK, response{
-		Available:   supply.AvailableSupply(time.Now()),
-		Circulating: supply.CirculatingSupply(time.Now()),
+		Available:   supply.AvailableSupply(t),
+		Circulating: supply.CirculatingSupply(t),
+	})
+}
+
+// getSupply responds with the supply of utia at the given date.
+func getSupplyByDate(c *gin.Context) {
+	date := c.Param("date")
+	t, err := time.Parse("2006-01-02", date)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid date parameter. Date must be in the format YYYY-MM-DD.",
+		})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, response{
+		Available:   supply.AvailableSupply(t),
+		Circulating: supply.CirculatingSupply(t),
 	})
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/supply", getSupply)
+	router.GET("/supply/:date", getSupplyByDate)
 	router.Run("localhost:8080")
 }
