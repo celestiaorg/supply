d2b5d8c66f0fd8c01cc8a6f348fcf29391696523
Rootul Patel <rootulp@gmail.com>
2023-10-29 10:38:14 +0100

    refactor: use more dates

--- available_test.go
+++ available_test.go
@@ -13,11 +13,11 @@ func Test_availableSupply(t *testing.T) {
 		want int64
 	}
 	testCases := []testCase{
-		{time.Date(2023, time.October, 31, 0, 0, 0, 0, time.UTC), 0},                    // before TGE
-		{time.Date(2023, time.October, 31, 14, 0, 0, 0, time.UTC), 266_986_177_750_000}, // TGE
-		{time.Date(2023, time.November, 1, 14, 0, 0, 0, time.UTC), 267_388_879_606_848},
-		{time.Date(2024, time.October, 31, 14, 0, 0, 0, time.UTC), 592_531_409_126_425},
-		{time.Date(2025, time.October, 31, 14, 0, 0, 0, time.UTC), 1_032_535_327_799_419},
+		{beforeTGE, 0},
+		{TGE, 266_986_177_750_000},
+		{oneDayAfterTGE, 267_388_879_606_848},
+		{oneYearAfterTGE, 592_531_409_126_425},
+		{twoYearsAfterTGE, 1_032_535_327_799_419},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.time.String(), func(t *testing.T) {
