30514f39f4b79cd265d60521dd5d5f4d39b29d73
Rootul Patel <rootulp@gmail.com>
2023-10-28 22:19:19 -0400

    refactor: investorsCirculating

--- circulating.go
+++ circulating.go
@@ -12,6 +12,7 @@ const (
 )
 
 var TGE = time.Date(2023, time.October, 31, 14, 0, 0, 0, time.UTC)
+var oneYearAfterTGE = TGE.AddDate(1, 0, 0)
 
 // circulatingSupply returns the circulating supply of utia at the given time.
 func circulatingSupply(t time.Time) int64 {
@@ -41,11 +42,13 @@ func ecosystemAllocationCirculating(t time.Time) int64 {
 }
 
 func investorsCirculating(t time.Time) int64 {
-	if t.Before(TGE) {
+	if t.Before(oneYearAfterTGE) {
 		return 0
 	}
 	daysSinceGenesis := int64(t.Sub(TGE).Hours() / 24)
-	return int64(min(investorsTotal/3+investorsTotal*2/3/365*(daysSinceGenesis-365), investorsTotal))
+	// 1/3 of total unlocks in chunk at TGE + 1 year.
+	// Remaining 2/3 of total unlocks from TGE + 1 year to TGE + 2 years.
+	return min(investorsTotal/3+investorsTotal*2/3/365*(daysSinceGenesis-365), investorsTotal)
 }
 
 func coreContributorsCirculating(t time.Time) int64 {
--- circulating_test.go
+++ circulating_test.go
@@ -27,3 +27,24 @@ func Test_circulatingSupply(t *testing.T) {
 		})
 	}
 }
+
+func Test_investorsCirculating(t *testing.T) {
+	type testCase struct {
+		time time.Time
+		want int64
+	}
+	testCases := []testCase{
+		{time.Date(2019, 1, 1, 0, 0, 0, 0, time.UTC), 0},                                // before TGE day
+		{time.Date(2023, time.October, 31, 0, 0, 0, 0, time.UTC), 0},                    // before TGE hour
+		{time.Date(2023, time.October, 31, 14, 0, 0, 0, time.UTC), 0},                   // at TGE
+		{time.Date(2023, time.November, 1, 14, 0, 0, 0, time.UTC), 0},                   // one day after TGE
+		{time.Date(2024, time.October, 31, 14, 0, 0, 0, time.UTC), 119_212_799_030_136}, // one year after TGE 1/3 of total unlocks
+		{time.Date(2025, time.October, 31, 14, 0, 0, 0, time.UTC), 355_689_414_000_000}, // two years after TGE total finishes unlock
+	}
+	for _, tc := range testCases {
+		t.Run(tc.time.String(), func(t *testing.T) {
+			got := investorsCirculating(tc.time)
+			require.Equal(t, tc.want, got)
+		})
+	}
+}
