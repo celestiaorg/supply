9e857d87cc91013574480ac59abe787e1fab6a08
Rootul Patel <rootulp@gmail.com>
2023-11-09 11:03:45 +0100

    feat: plaintext API for circulating supply

--- main.go
+++ main.go
@@ -1,51 +1,32 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
 	"time"
 
 	"github.com/celestiaorg/supply/internal"
 	"github.com/gin-gonic/gin"
 )
 
-type response struct {
-	// Available is the total supply of utia available at the given time.
-	Available int64 `json:"available"`
-	// Circulating is the circulating supply of utia available at the given time.
-	Circulating int64 `json:"circulating"`
-}
+const utiaPerTia = 100_000
 
-// getSupply responds with the current supply of utia.
-func getSupply(c *gin.Context) {
+func getCirculating(c *gin.Context) {
 	t := time.Now()
-	c.IndentedJSON(http.StatusOK, response{
-		Available:   internal.AvailableSupply(t),
-		Circulating: internal.CirculatingSupply(t),
-	})
-}
-
-// getSupply responds with the supply of utia at the given date.
-func getSupplyByDate(c *gin.Context) {
-	date := c.Param("date")
-	t, err := time.Parse("2006-01-02", date)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "Invalid date parameter. Date must be in the format YYYY-MM-DD.",
-		})
-		return
-	}
-	c.IndentedJSON(http.StatusOK, response{
-		Available:   internal.AvailableSupply(t),
-		Circulating: internal.CirculatingSupply(t),
-	})
+	circulating := internal.CirculatingSupply(t)
+	c.String(200, formatTia(circulating))
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/supply", getSupply)
-	router.GET("/supply/:date", getSupplyByDate)
+	router.GET("/supply/circulating", getCirculating)
+	// router.GET("/supply/total", getTotal)
 	err := router.Run("0.0.0.0:8080")
 	if err != nil {
 		panic(err)
 	}
+}
+
+func formatTia(utia int64) string {
+	tia := float64(utia) / utiaPerTia
+	return fmt.Sprintf("%f", tia)
 }
